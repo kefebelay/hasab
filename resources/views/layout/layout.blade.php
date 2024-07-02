1f60a53993aa72956c3ad7aff88044cfe53a3b1a
kefebelay <kefebelay14@gmail.com>
2024-07-02 16:27:36 +0300

    hot reload

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css') @vite('resources/js/app.js')
 </head>
 
 <body class="bg-gray-100">
